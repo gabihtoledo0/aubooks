6f779c8dd950a093e479ea6db95972cf8b0cfbca
gabihtoledo0 <gabitoledo60@gmail.com>
2022-05-17 20:59:58 -0300

    tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aubooks/pages/login/login.dart';
+import 'package:aubooks/pages/tela_inicial/primary_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: PrimaryScreen(),
     );
   }
 }
