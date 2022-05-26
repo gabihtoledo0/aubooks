e743be8be594d666014c299ff3318ca453a5874f
gabihtoledo0 <gabitoledo60@gmail.com>
2022-05-26 20:16:42 -0300

    finalizado tela de minha lista

--- lib/pages/home/homepage_navigation.dart
+++ lib/pages/home/homepage_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:aubooks/pages/home/homepage_screen.dart';
+import 'package:aubooks/pages/home/my_list.dart';
 
 class HomePageNavigation extends StatefulWidget {
   const HomePageNavigation({Key? key}) : super(key: key);
@@ -15,18 +16,7 @@ class _HomePageNavigation extends State<HomePageNavigation> {
   Widget build(BuildContext context){
     final textTheme = Theme.of(context).textTheme;
     final tabs = [
-      ListView(children: const <Widget>[
-        Text(
-          "minha lista",
-          style: TextStyle(
-              fontFamily: 'Sansation',
-              fontSize: 26,
-              fontWeight: FontWeight.w300,
-              color: Color(0xFFFFFFFF)
-          ),
-          textAlign: TextAlign.center,
-        )
-      ]),
+      const MyList(),
       const HomePageScreen(),
       ListView(children: const <Widget>[
         Text(
