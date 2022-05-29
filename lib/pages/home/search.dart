3adc5d4be6655f316e3e4527d4e8c6f0906a889b
gabihtoledo0 <gabitoledo60@gmail.com>
2022-05-29 14:28:57 -0300

    finalizado buscar da home

--- lib/pages/home/search.dart
+++ lib/pages/home/search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:aubooks/components/logo.dart';
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:aubooks/components/box_audios.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -9,52 +10,62 @@ class Search extends StatefulWidget {
 }
 
 class _Search extends State<Search> {
-  bool _searchBoolean = false; //add
+  TextEditingController textController = TextEditingController();
+  final _scrollController = ScrollController();
 
   @override
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF39403E),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 20, left: 20, top: 20),
-            child: Container(
-              width: double.infinity,
-              height: 40,
-              padding: const EdgeInsets.only(left: 18),
-              decoration: BoxDecoration(
-                  color: Colors.green[50],
-                  borderRadius: BorderRadius.circular(5)),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    suffixIcon: !_searchBoolean ?
-                    IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () {
-                          setState(() {
-                            _searchBoolean = true;
-                          });
-                        })
-                    :
-                    IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () {
-                          setState(() {
-                            _searchBoolean = false;
-                          });
-                        }
-                    ),
-                    hintText: 'Autor ou título',
-                    border: InputBorder.none),
+    return CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverPadding(
+              padding: EdgeInsets.only(
+                  right: 24, left: 24, top: 14
+              ),
+              sliver: SliverToBoxAdapter(
+                child: AnimSearchBar(
+                  width: 400,
+                  textController: textController,
+                  rtl: true,
+                  helpText: "Título, Autor ou Gênero",
+                  onSuffixTap: () {
+                    setState(() {
+                      textController.clear();
+                    });
+                  },
+                ),
+              ),
+          ),
+          const SliverPadding(
+            padding: EdgeInsets.only(left: 24, bottom: 18),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                'Resultados para " "',
+                style: TextStyle(
+                    fontFamily: 'Sansation',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFFFFFFFF)
                 ),
               ),
             ),
           ),
-        ],
-      )
+          SliverPadding(
+          padding: const EdgeInsets.only(right: 20, left: 20, bottom: 30),
+            sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0
+            ),
+            delegate: SliverChildBuilderDelegate(
+            (context, index) => BookGridItem(),
+            childCount: 4,
+            ),
+          ),
+          ),
+      ],
     );
   }
 }
